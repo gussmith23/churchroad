56452ca3a44c2f3571d1fefc5b9fe9c2e9804dfd
Gus Smith <guscomps@gmail.com>
2024-02-18 15:12:15 -0800

    Format .cfg file

--- fmt.sh
+++ fmt.sh
@@ -2,4 +2,9 @@
 
 set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# Rust formatting.
 cargo fmt --manifest-path "$SCRIPT_DIR"/Cargo.toml
+
+# Python formatting.
+black "$SCRIPT_DIR"/yosys-plugin/tests/lit.cfg
