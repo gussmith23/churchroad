a2641d30b56b815aac06a725938c4b8ecd927123
Gus Smith <guscomps@gmail.com>
2024-02-17 17:24:11 -0800

    Fix fmt.sh

--- fmt.sh
+++ fmt.sh
@@ -2,6 +2,4 @@
 
 set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-raco fmt -i "$SCRIPT_DIR"/racket/*.rkt
-raco fmt -i "$SCRIPT_DIR"/bin/*.rkt
-cargo fmt --manifest-path "$SCRIPT_DIR"/rust/Cargo.toml
+cargo fmt --manifest-path "$SCRIPT_DIR"/Cargo.toml
