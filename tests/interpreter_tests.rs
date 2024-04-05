e8befd2153cbbd68ddd6b1982a3b310536e15baf
Andrew Cheung <acheung8@uw.edu>
2024-04-04 19:15:04 -0700

    Swap LUT6 with simple ALU test for now

--- tests/interpreter_tests.rs
+++ tests/interpreter_tests.rs
@@ -4,8 +4,7 @@ use churchroad::{interpret, InterpreterResult};
 
 #[test]
 fn interpreter_returns_bv() {
-    // TODO(@ninehusky): This will fail right now, because the PR for $shiftx hasn't been merged yet.
-    let filename = "tests/interpreter_tests/lut6.egg";
+    let filename = "tests/interpreter_tests/ALU.egg";
     let program = std::fs::read_to_string(filename).unwrap();
     let result = interpret(program, 2);
     assert_eq!(result, InterpreterResult::Bitvector(0, 0));
