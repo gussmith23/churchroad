d37b3c6734fae2e69ccee00613f09bf1eb8be164
Andrew Cheung <acheung8@uw.edu>
2024-04-22 15:13:21 -0700

    Fix concat semantics, remove bitwidth extensions

---
+++ yosys-plugin/tests/concat.sv
@@ -0,0 +1,23 @@
+// RUN: $YOSYS -q -m $CHURCHROAD_DIR/yosys-plugin/churchroad.so \
+// RUN:   -p 'read_verilog -sv %s; prep -top test; write_lakeroad' \
+// RUN:   | FileCheck %s
+
+module test(input a, b, output[1:0] out);
+  assign out = {a, b};
+endmodule
+
+// CHECK: (let v0 (Wire "v0" 1))
+// CHECK: (let v1 (Wire "v1" 1))
+// CHECK: (let v2 (Wire "v2" 2))
+// CHECK: (union v2 (Op2 (Concat) v0 v1))
+// CHECK: (let a (Var "a" 1))
+// CHECK: (IsPort "" "a" (Input) a)
+// CHECK: (union v0 a)
+// CHECK: (let b (Var "b" 1))
+// CHECK: (IsPort "" "b" (Input) b)
+// CHECK: (union v1 b)
+// CHECK: (let out v2)
+// CHECK: (IsPort "" "out" (Output) out)
+// CHECK: (delete (Wire "v0" 1))
+// CHECK: (delete (Wire "v1" 1))
+// CHECK: (delete (Wire "v2" 2))
