d0cee052ea2154b1fbb858331f45d2a2e1e1ad3a
Gus Smith <guscomps@gmail.com>
2024-04-21 18:39:28 -0700

    Add basic to-verilog compilation

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@ use std::{
 };
 
 use egglog::{
-    ast::{Literal, Symbol},
+    ast::{parse, Literal, Symbol},
     constraint::{SimpleTypeConstraint, TypeConstraint},
     sort::{FromSort, I64Sort, IntoSort, Sort, VecSort},
     util::IndexMap,
     ArcSort, EGraph, PrimitiveLike, Term, TermDag, Value,
 };
+use egraph_serialize::{Class, ClassId, Node};
 
 #[derive(Default)]
 pub struct AnythingExtractor;
@@ -30,6 +31,267 @@ impl AnythingExtractor {
     }
 }
 
+pub fn to_verilog_egraph_serialize(
+    egraph: &egraph_serialize::EGraph,
+    choices: &IndexMap<egraph_serialize::ClassId, egraph_serialize::NodeId>,
+    clk_name: &str,
+) -> String {
+    // let mut wires = HashMap::default();
+
+    fn id_to_wire_name(id: &ClassId) -> String {
+        format!("wire_{}", id)
+    }
+
+    let mut inputs = String::new();
+    let mut logic_declarations = String::new();
+    let mut registers = String::new();
+    let mut module_declarations = String::new();
+
+    // Hack: just add all IDs to the queue.
+    let mut queue: Vec<ClassId> = choices.keys().cloned().collect();
+    let mut done = HashSet::new();
+
+    while let Some(id) = queue.pop() {
+        done.insert(id.clone());
+        let term = &egraph[&choices[&id]];
+
+        let op = &term.op;
+        match op.as_str() {
+            // Things to ignore.
+            //
+            // Ignore the Unit.
+            "()" |
+            // Ignore the nodes for the ops themselves.
+            "Or" |
+            "And" |
+            "Add" |
+            "Shr" |
+            "Eq" |
+            "Xor" |
+            "Reg" => (),
+            // Ignore integer literals.
+            v if v.parse::<i64>().is_ok() => (),
+
+            "Op1" => {
+                let op_node = &egraph[&term.children[0]];
+                match op_node.op.as_str() {
+                    "Reg" => {
+                        let default_val = egraph[&op_node.children[0]].op.parse::<i64>().unwrap();
+                        let d_id = &egraph[&term.children[1]].eclass;
+
+
+                    logic_declarations.push_str(
+                        format!(
+                            "logic {this_wire} = {default};\n",
+                            this_wire = id_to_wire_name(&id),
+                            default = default_val
+                        )
+                        .as_str(),
+                    );
+
+                    registers.push_str(&format!(
+                        "always @(posedge {clk_name}) begin
+                            {this_wire} <= {d};
+                        end\n",
+                        // clk = id_to_wire_name(clk_id),
+                        this_wire = id_to_wire_name(&id),
+                        d = id_to_wire_name(&d_id)
+                    ));
+
+                    if !done.contains(&d_id) {
+                        queue.push(d_id.clone());
+                    }
+                    // if !done.contains(&clk_id) {
+                    //     queue.push(clk_id);
+                    // }
+                    }
+                v => todo!("{:?}", v),
+
+                }
+
+            }
+
+            // Term::Lit(Literal::Int(v)) => {
+            //     logic_declarations.push_str(&format!(
+            //         "logic [31:0] {this_wire} = {val};\n",
+            //         this_wire = id_to_wire_name(id),
+            //         val = v
+            //     ));
+            // }
+            // Term::Var(_) => todo!(),
+            // Term::App(s, v) => match (s.as_str(), v.as_slice()) {
+            //     ("Reg", &[default_id, clk_id, d_id]) => {
+            //         let default_val = match term_dag.get(default_id) {
+            //             Term::Lit(Literal::Int(default_val)) => default_val,
+            //             _ => panic!(),
+            //         };
+
+            //         logic_declarations.push_str(
+            //             format!(
+            //                 "logic {this_wire} = {default};\n",
+            //                 this_wire = id_to_wire_name(id),
+            //                 default = default_val
+            //             )
+            //             .as_str(),
+            //         );
+
+            //         registers.push_str(&format!(
+            //             "always @(posedge {clk}) begin
+            //                 {this_wire} <= {d};
+            //             end\n",
+            //             clk = id_to_wire_name(clk_id),
+            //             this_wire = id_to_wire_name(id),
+            //             d = id_to_wire_name(d_id)
+            //         ));
+
+            //         if !done.contains(&d_id) {
+            //             queue.push(d_id);
+            //         }
+            //         if !done.contains(&clk_id) {
+            //             queue.push(clk_id);
+            //         }
+            //     }
+            //     ("Var", [name_id, bw_id]) => {
+            //         let name = match term_dag.get(*name_id) {
+            //             Term::Lit(Literal::String(name)) => name,
+            //             _ => panic!(),
+            //         };
+            //         let bw = match term_dag.get(*bw_id) {
+            //             Term::Lit(Literal::Int(bw)) => bw,
+            //             _ => panic!(),
+            //         };
+
+            //         inputs.push_str(
+            //             format!("input [{bw}-1:0] {name};\n", bw = bw, name = name).as_str(),
+            //         );
+
+            //         logic_declarations.push_str(
+            //             format!(
+            //                 "logic [{bw}-1:0] {this_wire} = {name};\n",
+            //                 bw = bw,
+            //                 this_wire = id_to_wire_name(id),
+            //                 name = name
+            //             )
+            //             .as_str(),
+            //         );
+            //     }
+            //     ("Mux", []) => (),
+            //     ("LUT4", []) => (),
+            //     ("Or", []) => (),
+            //     ("Bitvector", [_]) => (),
+            //     ("Eq", []) => (),
+            //     ("BV", [val_id, bw_id]) => {
+            //         let val = match term_dag.get(*val_id) {
+            //             Term::Lit(Literal::Int(val)) => val,
+            //             _ => panic!(),
+            //         };
+            //         let bw = match term_dag.get(*bw_id) {
+            //             Term::Lit(Literal::Int(bw)) => bw,
+            //             _ => panic!(),
+            //         };
+            //         logic_declarations.push_str(
+            //             format!(
+            //                 "logic [{bw}-1:0] {this_wire} = {bw}'d{val};\n",
+            //                 bw = bw,
+            //                 this_wire = id_to_wire_name(id),
+            //                 val = val
+            //             )
+            //             .as_str(),
+            //         );
+            //     }
+            //     ("Extract", [hi_id, lo_id, expr_id]) => {
+            //         let hi = match term_dag.get(*hi_id) {
+            //             Term::Lit(Literal::Int(hi)) => hi,
+            //             _ => panic!(),
+            //         };
+            //         let lo = match term_dag.get(*lo_id) {
+            //             Term::Lit(Literal::Int(lo)) => lo,
+            //             _ => panic!(),
+            //         };
+            //         logic_declarations.push_str(&format!(
+            //             "logic {this_wire} = {expr}[{hi}:{lo}];\n",
+            //             hi = hi,
+            //             lo = lo,
+            //             this_wire = id_to_wire_name(id),
+            //             expr = id_to_wire_name(*expr_id),
+            //         ));
+
+            //         if !done.contains(&expr_id) {
+            //             queue.push(*expr_id);
+            //         }
+            //     }
+            //     ("Concat", [expr0_id, expr1_id]) => {
+            //         logic_declarations.push_str(&format!(
+            //             "logic {this_wire} = {{ {expr0}, {expr1} }};\n",
+            //             this_wire = id_to_wire_name(id),
+            //             expr0 = id_to_wire_name(*expr0_id),
+            //             expr1 = id_to_wire_name(*expr1_id),
+            //         ));
+
+            //         if !done.contains(&expr0_id) {
+            //             queue.push(*expr0_id);
+            //         }
+            //         if !done.contains(&expr1_id) {
+            //             queue.push(*expr1_id);
+            //         }
+            //     }
+            //     ("ZeroExtend", [expr_id, bw_id]) => {
+            //         let bw = match term_dag.get(*bw_id) {
+            //             Term::Lit(Literal::Int(bw)) => bw,
+            //             _ => panic!(),
+            //         };
+            //         logic_declarations.push_str(&format!(
+            //             "logic {this_wire} = {{ {bw}'d0, {expr} }};\n",
+            //             this_wire = id_to_wire_name(id),
+            //             bw = bw,
+            //             expr = id_to_wire_name(*expr_id),
+            //         ));
+
+            //         if !done.contains(&expr_id) {
+            //             queue.push(*expr_id);
+            //         }
+            //     }
+            //     ("Sketch1", [op_id, expr_id])
+            //         if match term_dag.get(*op_id) {
+            //             Term::App(s, v) => s.as_str() == "LUT4" && v.is_empty(),
+            //             _ => false,
+            //         } =>
+            //     {
+            //         logic_declarations.push_str(&format!(
+            //             "logic {this_wire};\n",
+            //             this_wire = id_to_wire_name(id),
+            //         ));
+
+            //         module_declarations.push_str(&format!(
+            //             "lut4 lut4_{id} (.in({expr}), .out({y}));\n",
+            //             id = id,
+            //             expr = id_to_wire_name(*expr_id),
+            //             y = id_to_wire_name(id),
+            //         ));
+
+            //         if !done.contains(&expr_id) {
+            //             queue.push(*expr_id);
+            //         }
+            //     }
+            //     _ => todo!("{:?}", (s, v)),
+            // },
+            _ => todo!("{:?}", &term),
+        }
+    }
+
+    format!(
+        "module top({inputs});
+            {inputs}
+            {logic_declarations}
+            {registers}
+            {module_declarations}
+        endmodule",
+        inputs = inputs,
+        logic_declarations = logic_declarations,
+        registers = registers,
+        module_declarations = module_declarations,
+    )
+}
 pub fn to_verilog(term_dag: &TermDag, id: usize) -> String {
     // let mut wires = HashMap::default();
 
@@ -866,7 +1128,6 @@ mod tests {
             "#).unwrap();
     }
 
-
     #[test]
     fn extract_cycle() {
         let mut egraph = EGraph::default();
@@ -883,8 +1144,22 @@ mod tests {
             )
             .unwrap();
 
-    
-            let serialized = egraph.serialize(SerializeConfig::default());
-            dbg!(AnythingExtractor::default().extract(&serialized, &[]));
+        let serialized = egraph.serialize(SerializeConfig::default());
+        let out = (AnythingExtractor::default().extract(&serialized, &[]));
+
+        // TODO(@gussmith23) terrible assertion, but it's a start.
+        assert_eq!(
+            "module top();
+            
+            logic wire_6 = 0;
+
+            always @(posedge clk) begin
+                            wire_6 <= wire_6;
+                        end
+
+            
+        endmodule",
+            to_verilog_egraph_serialize(&serialized, &out, "clk")
+        );
     }
 }
