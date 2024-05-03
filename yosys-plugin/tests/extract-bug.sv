4faeaff754bc5e7b49ffb4d26b50d2728de5f1e8
Gus Smith <guscomps@gmail.com>
2024-05-03 10:21:22 -0700

    Fix test

--- yosys-plugin/tests/extract-bug.sv
+++ yosys-plugin/tests/extract-bug.sv
@@ -14,8 +14,7 @@ endmodule
 
 // CHECK: (let v0 (Wire "v0" 2))
 // CHECK: (let v1 (Wire "v1" 1))
-// CHECK: (let v2 (Op1 (Extract 0 0) v0))
-// CHECK: (union v1 (Op1 (Extract 1 1) v2))
+// CHECK: (union v1 (Op1 (Extract 1 1) v0))
 // CHECK: (let in (Var "in" 2))
 // CHECK: (IsPort "" "in" (Input) in)
 // CHECK: (union v0 in)
